092911f7067f3416e1447488731bbea2efb5fa31
William Vitorino <williamvitorino3@gmail.com>
2020-07-18 11:48:53 -0300

    Bootstrap + axios
    Signed-off-by: William Vitorino <williamvitorino3@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.prototype.$http = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: {
+    accept: 'application/vnd.github.v3+json'
+  }
+})
 
 new Vue({
   router,
