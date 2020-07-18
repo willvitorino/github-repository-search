54ae87db3c272dfa8a38df61a959801097797be7
William Vitorino <williamvitorino3@gmail.com>
2020-07-17 21:40:28 -0300

    css global
    Signed-off-by: William Vitorino <williamvitorino3@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
 }
 
-#nav {
-  padding: 30px;
+input[type=text], button {
+  border-radius: 0px;
+  border: solid black 2px;
+  height: 2rem;
+  text-align: center;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+button {
+  background-color: black;
+  color: white;
+  height: 2.25rem;
+  padding: 0px 1rem;
 }
 </style>
