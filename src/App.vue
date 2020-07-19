5196ee90df62b3fc71df4445f5b5a699b646097e
William Vitorino <williamvitorino3@gmail.com>
2020-07-18 23:37:36 -0300

    Tela de loading
    Signed-off-by: William Vitorino <williamvitorino3@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div v-if="loading" class="d-flex flex-row justify-content-center align-items-center container-spinner">
+       <b-icon icon="three-dots" animation="cylon throb" font-scale="10"></b-icon>
+    </div>
+    <router-view v-show="!loading" />
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    loading () {
+      return this.$store.getters.loading
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import 'node_modules/bootstrap/scss/bootstrap';
 @import 'node_modules/bootstrap-vue/src/index.scss';
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,700;1,300;1,400;1,700&display=swap');
 
 * {
   margin: 0;
@@ -16,6 +30,8 @@
   width: 100vw;
   height: 100vh;
   background-color: white;
+
+  overflow-x: hidden;
 }
 
 input[type=text], button {
@@ -30,5 +46,11 @@ button {
   color: white;
   height: 2.25rem;
   padding: 0px 1rem;
+}
+.container-spinner {
+  height: 100%;
+}
+.pointer {
+  cursor: pointer;
 }
 </style>
