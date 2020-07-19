d8ead27ec024395ca5f200554c7e9af77a789225
William Vitorino <williamvitorino3@gmail.com>
2020-07-19 00:01:44 -0300

    Esqueleto da página de Detail
    Signed-off-by: William Vitorino <williamvitorino3@gmail.com>

---
+++ src/views/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="detail" class="form-control m-0">
+    {{ $route.params.id }}
+  </section>
+</template>
+
+<script>
+export default {
+  beforeCreate () {
+    const { id } = this.$router.params
+    if (!id) {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
